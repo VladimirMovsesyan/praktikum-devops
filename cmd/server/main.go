f174abee46b1be438d97207585e06b6ed29a9f88
vladimirmovsesyan <vladimirmovsesyan-dev@yandex.ru>
2023-03-11 01:41:19 +0300

    Local storage based on repository pattern added. Updating local repository handler added.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,52 @@
 package main
 
-func main() {}
+import (
+	"github.com/VladimirMovsesyan/praktikum-devops/internal/metrics"
+	"github.com/VladimirMovsesyan/praktikum-devops/internal/repository"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func updateStorageHandler(storage repository.MetricRepository) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		urlSlice := strings.Split(r.URL.Path, "/")
+		if len(urlSlice) != 5 {
+			rw.WriteHeader(http.StatusNotFound)
+			return
+		}
+		kind, name := urlSlice[2], urlSlice[3]
+		newMetric := metrics.Metric{}
+
+		switch kind {
+		case "gauge":
+			value, err := strconv.Atoi(urlSlice[4])
+			if err != nil {
+				log.Println(err)
+				rw.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			newMetric = metrics.NewMetricGauge(name, metrics.Gauge(value))
+		case "counter":
+			value, err := strconv.Atoi(urlSlice[4])
+			if err != nil {
+				log.Println(err)
+				rw.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			newMetric = metrics.NewMetricCounter(name, metrics.Counter(value))
+		default:
+			rw.WriteHeader(http.StatusNotImplemented)
+			return
+		}
+		storage.Update(newMetric)
+		rw.WriteHeader(http.StatusOK)
+	}
+}
+
+func main() {
+	storage := &repository.MemStorage{}
+	http.HandleFunc("/update/", updateStorageHandler(storage))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
